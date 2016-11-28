35f5c4f50976379ee8d39b3ad2f97a90e6d20232
Eduard Pikhno <i@emdc.ru>
2016-11-28 20:35:30 +0400

    Добавлена заготовка Assert

---
+++ src/assert.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <cstdint>
+#include <cstddef>
+#include <functional>
+#include <exception>
+
+namespace foreman {
+	class AssertException : public std::exception {
+	public:
+		AssertException() : exception() {}
+	};
+
+	class Assert {
+	public:
+		static void test(std::function<bool()> fcn);
+	private:
+		Assert() {}
+	};
+
+	void Assert::test(std::function<bool()> fcn) {
+		if (!fcn()) {
+			throw AssertException();
+		}
+	}
+}
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <cstdio>
+#include <cstdint>
+
+#include "assert.hpp"
+
+int32_t main() {
+	try {
+		foreman::Assert::test([]() -> bool { return false; });
+	} catch (foreman::AssertException& e) {
+		printf("Assert failed!\n");
+	}
+	
+	fflush(stdin);
+	getchar();
+
+	return 0;
+}
