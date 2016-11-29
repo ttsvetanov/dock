d95fbcd9b6c7f6f62f3bac3f04b3e1dad62ac818
Eduard Pikhno <i@emdc.ru>
2016-11-29 20:43:50 +0400

    Завершен модуль Assert

--- src/assert.hpp
+++ src/assert.hpp
@@ -7,52 +7,117 @@
 #include <exception>
 
 namespace foreman {
-	class AssertException : public std::exception {
-	public:
-		AssertException() : exception("Assert failed!") {}
-	};
-
+	// Simple assert library for unit-testing engine
 	class Assert {
 	public:
-		static void test(std::function<bool()> fcn);
+		// General assert exception without specialized message 
+		class Exception : public std::exception {
+		public:
+			Exception() : exception("Assert failed!") {}
+		};
+
+		// API
+		static void isTrue(std::function<bool()> fcn);
 
 		template<typename T>
-		static void isEquals(const T a, const T b, std::function<bool(const T&, const T&)> compareFcn = nullptr);
+		static void isEquals(const T a, const T b, std::function<bool(const T, const T)> compareFcn = defaultEqualsFunction<T>);
+
+		template<typename T>
+		static void isGreater(const T a, const T b, std::function<bool(const T, const T)> compareFcn = defaultGreaterFunction<T>);
+
+		template<typename T>
+		static void isLess(const T a, const T b, std::function<bool(const T, const T)> compareFcn = defaultLessFunction<T>);
 	private:
+		// Methods
 		Assert() {}
+
+		template<typename T>
+		static void generalAssertMethod(const T a, const T b, std::function<bool(const T, const T)> compareFcn);
+
+		// Constants
+		template<typename T>
+		static const std::function<bool(const T, const T)> defaultEqualsFunction;
+
+		template<typename T>
+		static const std::function<bool(const T, const T)> defaultGreaterFunction;
+
+		template<typename T>
+		static const std::function<bool(const T, const T)> defaultLessFunction;
 	};
 
 	// -------------------------------------------------------------------------------------------------------------------
 
-	void Assert::test(std::function<bool()> fcn) {
+	// Definitions of static constants
+	template<typename T>
+	const std::function<bool(const T, const T)> Assert::defaultEqualsFunction =
+		[](const T a, const T b) -> bool { return a == b; };
+
+	template<typename T>
+	const std::function<bool(const T, const T)> Assert::defaultGreaterFunction =
+		[](const T a, const T b) -> bool { return a > b; };
+
+	template<typename T>
+	const std::function<bool(const T, const T)> Assert::defaultLessFunction =
+		[](const T a, const T b) -> bool { return a < b; };
+
+	// -------------------------------------------------------------------------------------------------------------------
+
+	void Assert::isTrue(std::function<bool()> fcn) {
 		if (!fcn()) {
-			throw AssertException();
+			throw Assert::Exception();
 		}
 	}
 
+	// -------------------------------------------------------------------------------------------------------------------
+
 	template<typename T>
-	void Assert::isEquals(const T a, const T b, std::function<bool(const T&, const T&)> compareFcn) {
-		if (compareFcn) {
-			if (!compareFcn(a, b)) {
-				throw AssertException();
-			}
-		} else {
-			if (a != b) {
-				throw AssertException();
-			}
-		}
+	void Assert::isEquals(const T a, const T b, std::function<bool(const T, const T)> compareFcn) {
+		Assert::generalAssertMethod(a, b, compareFcn);
 	}
 
 	template<>
-	void Assert::isEquals<const char*>(const char* const a, const char* const b, std::function<bool(const char* const&, const char* const&)> compareFcn) {
+	void Assert::isEquals<const char*>(const char* const a, const char* const b, 
+									std::function<bool(const char* const, const char* const)> compareFcn) {
 		if (compareFcn) {
 			if (!compareFcn(a, b)) {
-				throw AssertException();
+				throw Assert::Exception();
 			}
 		} else {
-			if (strcmp(a, b)) {
-				throw AssertException();
+			std::size_t strALength = std::strlen(a);
+			std::size_t strBLength = std::strlen(b);
+			if (strALength != strBLength) {
+				throw Assert::Exception();
 			}
+			if (std::strcmp(a, b)) {
+				throw Assert::Exception();
+			}
+		}
+	}
+
+	// -------------------------------------------------------------------------------------------------------------------
+
+	template<typename T>
+	void Assert::isGreater(const T a, const T b, std::function<bool(const T, const T)> compareFcn) {
+		Assert::generalAssertMethod(a, b, compareFcn);
+	}
+
+	// -------------------------------------------------------------------------------------------------------------------
+
+	template<typename T>
+	void Assert::isLess(const T a, const T b, std::function<bool(const T, const T)> compareFcn) {
+		Assert::generalAssertMethod(a, b, compareFcn);
+	}
+
+	// -------------------------------------------------------------------------------------------------------------------
+
+	template<typename T>
+	void Assert::generalAssertMethod(const T a, const T b, std::function<bool(const T, const T)> compareFcn) {
+		if (compareFcn) {
+			if (!compareFcn(a, b)) {
+				throw Assert::Exception();
+			}
+		} else {
+			throw Assert::Exception();
 		}
 	}
 }
