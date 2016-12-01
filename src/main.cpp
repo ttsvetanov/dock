8530a6abab3507ef172e7d6ed5dbcefd664b9b4d
Eduard Pikhno <i@emdc.ru>
2016-12-01 20:29:19 +0400

    Начата работа над сериализатором в JSON

--- src/main.cpp
+++ src/main.cpp
@@ -19,22 +19,8 @@ Module(u8"Some module 1", {
     });
 });
 
-Module(u8"Some module 2", {
-    Test(u8"Some test 1", []() {
-        Assert::isTrue([]() -> bool { return true; });
-    });
-
-    Test(u8"Some test 2", []() {
-        Assert::isTrue([]() -> bool { return true; });
-    });
-
-    Test(u8"Some test 3", []() {
-        Assert::isTrue([]() -> bool { return false; });
-    });
-});
-
 int32_t main() {
     Core::getInstance().run();
 
-	return 0;
+    return 0;
 }
--- src/result.hpp
+++ src/result.hpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <cstddef>
 
+#include <vector>
 #include <functional>
 
 #include "assert.hpp"
@@ -20,18 +21,32 @@ namespace foreman {
         bool        isPassed;
     };
 
+    class ResultSerializer {
+    public:
+        virtual void serialize(std::vector<Result>& results) = 0;
+    };
+
+    class JsonResultSerializer : public ResultSerializer {
+    public:
+        virtual void serialize(std::vector<Result>& results) override;
+    };
+
+    // ----------------------------------------------------------------------------------------------------------------
+
     Result::Result(const char* _module, const char* _test, bool _isPassed) {
         this->module = _module;
         this->test = _test;
         this->isPassed = _isPassed;
     }
 
-    class ResultSerializer {
-    public:
-        //static void serialize();
-    private:
-        ResultSerializer() = delete;
-    };
+    void JsonResultSerializer::serialize(std::vector<Result>& results) {
+        using json = nlohmann::json;
 
-    // ----------------------------------------------------------------------------------------------------------------
+        json outJson;
+
+        outJson[u8"results"] = nullptr;
+        for (auto iter = results.begin(); iter != results.end(); ++iter) {
+            
+        }
+    }
 }
